6f75f1c0d8496d9dc6d7c7a11ab81b9fd16b549b
Miguel <miguel.oliveira@uni.lu>
2018-10-12 10:02:30 +0200

    movie driver

---
+++ movie.py
@@ -0,0 +1,29 @@
+import pandas as pd
+PATH = "../rawData/movie_recommendations.xlsx"
+df = pd.read_excel(PATH)
+# find and remove duplicate movie entries (to avoid problems with unstack)
+#duplicates = df.index
+#df.index.drop_duplicates()
+
+print(df.head(10))
+print(df.groupby('Reviewer')['Rating'].mean)
+
+#df[df['Rating'] == df['Rating'].max().head(10)]
+
+df.sort_values('Rating', ascending = False, inplace = True)
+print(df.head(5).sample(1))
+
+df.set_index('Name', inplace=True)
+print(df.head(3))
+
+long = df.stack()
+print(long.shape)
+print(df.shape)
+print(long.head(10))
+long['Lion'] 
+
+short = long.head(15)
+wide = short.unstack(1) #unstack based on Gender (second column in the short)
+print(wide)
+
+
