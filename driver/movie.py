250f8d964003d48fa877db313bd9dbfdaecf263d
tranncl <tranncl@gmail.com>
2018-10-19 11:10:22 +0200

    Nicolas put CR code

--- driver/movie.py
+++ driver/movie.py
@@ -1,29 +1,28 @@
 import pandas as pd
 PATH = "../rawData/movie_recommendations.xlsx"
 df = pd.read_excel(PATH)
-# find and remove duplicate movie entries (to avoid problems with unstack)
-duplicates = df.index
-df.index.drop_duplicates()
+print("""
+    Kristians and Felipes Movie Recommender:
+    Enter your favourite genre or reviwer
+    and the program will recommend a movie.
+""")
 
-print(df.head(10))
-print(df.groupby('Reviewer')['Rating'].mean)
+#TODO: we need to ask for the reviewer
 
-#df[df['Rating'] == df['Rating'].max().head(10)]
+print("enter a genre (or leave it empty): ", end="")
+genre = input()
 
-df.sort_values('Rating', ascending = False, inplace = True)
-print(df.head(5).sample(1))
+if genre:
+    g = df[df['Genre'] == genre]
+else:
+    g = df
 
-df.set_index('Name', inplace=True)
-print(df.head(5))
 
-long = df.stack()
-print(long.shape)
-print(df.shape)
-print(long.head(10))
+if g.shape[0] > 0:
+    result = g.sort_values('Rating', ascending=False)
+    print(result.head(3).sample(1))
+else:
+    print("Sorry, no results!")
 
-short = long.head(15)
-print("short",short)
-wide = short.unstack(1) #unstack based on Gender (second column in the short)
-print(wide)
 
 
